ff2fd031595a7a1f0bb4830458400cd47012c3f8
Anderson Felipe Dias Domingos <139730074+Felipe-dmgs@users.noreply.github.com>
2026-07-20 20:44:45 -0300

    fix(settings): Duplicação do databases
    Corrige a duplicação da definição da database

--- config/settings.py
+++ config/settings.py
@@ -75,17 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": (
@@ -128,6 +117,6 @@ DATABASES = {
         'USER': os.getenv('DATABASE_USERNAME', 'myprojectuser'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DATABASE_PORT', 5432),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
 }
