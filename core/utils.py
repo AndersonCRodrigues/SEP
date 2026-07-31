dc57007f9244b4802f1d7d97fa9737269df920c6
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-07-30 23:48:10 -0300

    feat(group):adiçaõ de grupos e permissões

---
+++ core/utils.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import Group
+from .models import CustomUser
+
+
+MAPA_GRUPOS = {
+    CustomUser.Role.SUPERADMIN: "Superadmin",
+    CustomUser.Role.SUPERVISOR: "Supervisor",
+    CustomUser.Role.ADMIN: "Administration",
+    CustomUser.Role.PROFESSOR: "Professors",
+    CustomUser.Role.ALUNO: "Students",
+}
+
+
+def sincronizar_grupo(user):
+    nome_grupo = MAPA_GRUPOS.get(user.role)
+    if not nome_grupo:
+        return
+    grupo, _ = Group.objects.get_or_create(name=nome_grupo)
+    user.groups.set([grupo])
