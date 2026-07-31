e6bdb7fdc384e7b4dfaa157aa9ecbdf2e5ffa76b
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-07-30 23:51:51 -0300

    refactor:alterei o local dessa view,estava no core

--- professors/views.py
+++ professors/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import ListView
+from core.models import CustomUser
 
-# Create your views here.
+
+class GroupRequiredMixin(UserPassesTestMixin):
+    required_group = "Professors"
+
+    def test_func(self):
+        if not self.request.user.is_authenticated:
+            return False
+        return (
+            self.request.user.groups.filter(name=self.required_group).exists() 
+            or self.request.user.is_superuser
+        )
+
+
+class PainelProfessorView(GroupRequiredMixin, ListView):
+    model = CustomUser
+    template_name = "painel_professor.html"  # Nome exato da sua pasta templates
+    context_object_name = "alunos"
+
+    def get_queryset(self):
+        return CustomUser.objects.filter(role=CustomUser.Role.ALUNO)
