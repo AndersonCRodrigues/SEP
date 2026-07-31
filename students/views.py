9705c331339fd7d2061e95332ea1caa7ef90fee6
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-07-30 23:52:22 -0300

    feat:adição da view que direciona para a tela de estudantes

--- students/views.py
+++ students/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class GroupRequiredMixin(UserPassesTestMixin):
+    required_group = "Students"
+
+    def test_func(self):
+        if not self.request.user.is_authenticated:
+            return False
+        return (
+            self.request.user.groups.filter(name=self.required_group).exists() 
+            or self.request.user.is_superuser
+        )
+
+
+class PainelEstudanteView(GroupRequiredMixin, TemplateView):
+    template_name = "estudantes.html"  
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["estudante"] = self.request.user
+        return context
