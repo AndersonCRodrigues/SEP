56daf0de00732f47ab269fcbbfff7f192fffa1fa
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-07-30 23:43:06 -0300

    refactor:mudança de nome da variavel

--- administration/urls.py
+++ administration/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "administration" # Ajustado para bater com o nome da pasta e template
+app_name = "administration" 
 
 urlpatterns = [
     path("", views.PainelAdministracaoView.as_view(), name="painel"),
