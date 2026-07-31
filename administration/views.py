a4bb93f0dbb748ebcd6bd4d804413ef2a897715e
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-07-30 23:43:24 -0300

    feat: criação da view de admin

--- administration/views.py
+++ administration/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import ListView
+from core.models import CustomUser
 
-# Create your views here.
+
+class GroupRequiredMixin(UserPassesTestMixin):
+    required_group = "Administration"
+
+    def test_func(self):
+        if not self.request.user.is_authenticated:
+            return False
+        return (
+            self.request.user.groups.filter(name=self.required_group).exists() 
+            or self.request.user.is_superuser
+        )
+
+
+class PainelAdministracaoView(GroupRequiredMixin, ListView):
+    model = CustomUser
+    template_name = "painel_administracao.html"  
+    context_object_name = "atendimentos_ou_usuarios"
+
+    def get_queryset(self):
+        return CustomUser.objects.filter(
+            role__in=[CustomUser.Role.ALUNO, CustomUser.Role.PROFESSOR]
+        )
