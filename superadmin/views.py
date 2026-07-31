90709280d54326bad47be6f504c3f166caef9abe
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-07-30 23:53:06 -0300

    refactor:mudança da view,estava no core

--- superadmin/views.py
+++ superadmin/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import ListView
+from core.models import CustomUser
 
-# Create your views here.
+
+class SuperadminRequiredMixin(UserPassesTestMixin):
+    def test_func(self):
+        if not self.request.user.is_authenticated:
+            return False
+        return (
+            self.request.user.groups.filter(name="Superadmin").exists() 
+            or self.request.user.is_superuser
+        )
+
+
+class PainelSuperadminView(SuperadminRequiredMixin, ListView):
+    model = CustomUser
+    template_name = "painel_superadmin.html"  
+    context_object_name = "todos_usuarios"
+
+    def get_queryset(self):
+        return CustomUser.objects.all().order_by("-id")
