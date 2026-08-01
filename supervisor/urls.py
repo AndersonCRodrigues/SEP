4a1ad685ccdd14fe63582886a72d1c8a2fbaef70
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-01 20:44:17 -0300

    feat(refactor): corrigir ordem das views no arquivo urls.py

--- supervisor/urls.py
+++ supervisor/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = "supervisor"
 
 urlpatterns = [
-    path("", views.PainelSupervisorView.as_view(), name="painel"),
-    path("usuarios/", views.ListarUsuariosView.as_view(), name="listar_usuarios"), 
+    path("", views.HomeSupervisorView.as_view(), name="home"),
+    path("painel/", views.PainelSupervisorView.as_view(), name="painel"),
+    path("usuarios/", views.ListarUsuariosView.as_view(), name="listar_usuarios"),
+    path("register/", views.cadastrar_usuario, name="register"),
 ]
