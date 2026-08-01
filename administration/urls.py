f6a24a0099f8b53ddd484da6e984761269908772
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-01 20:38:41 -0300

    feat(home):adição da view home

--- administration/urls.py
+++ administration/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = "administration" 
+app_name = "administration"
 
 urlpatterns = [
-    path("", views.PainelAdministracaoView.as_view(), name="painel"),
+    path("", views.HomeAdministracaoView.as_view(), name="home"),
+    path("painel/", views.PainelAdministracaoView.as_view(), name="painel"),
 ]
