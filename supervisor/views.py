04467ea1d43caed6401143e0245accc16ab5c912
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-01 20:44:35 -0300

    feat(refactor): refatorar views e adicionar mensagens de sucesso no cadastro de usuários

--- supervisor/views.py
+++ supervisor/views.py
@@ -1,23 +1,18 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic import ListView
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, TemplateView
 from core.models import CustomUser
-
-
-class GroupRequiredMixin(UserPassesTestMixin):
-    required_group = "Supervisor"
-
-    def test_func(self):
-        if not self.request.user.is_authenticated:
-            return False
-        return (
-            self.request.user.groups.filter(name=self.required_group).exists() 
-            or self.request.user.is_superuser
-        )
+from core.mixins import GroupRequiredMixin
+from core.forms import CustomUserCreationForm
+from core.utils import sincronizar_grupo
 
 
 class PainelSupervisorView(GroupRequiredMixin, ListView):
+    required_group = "Supervisor"
     model = CustomUser
-    template_name = "painel_supervisor.html"  
+    template_name = "supervisor/supervisor_panel.html"
     context_object_name = "usuarios_listados"
 
     def get_queryset(self):
@@ -27,11 +22,39 @@ class PainelSupervisorView(GroupRequiredMixin, ListView):
 
 
 class ListarUsuariosView(GroupRequiredMixin, ListView):
+    required_group = "Supervisor"
     model = CustomUser
-    template_name = "listar_usuarios.html"  
+    template_name = "supervisor/list_users.html"
     context_object_name = "usuarios_listados"
 
     def get_queryset(self):
         return CustomUser.objects.exclude(
             role__in=[CustomUser.Role.SUPERADMIN, CustomUser.Role.SUPERVISOR]
         )
+
+
+class HomeSupervisorView(GroupRequiredMixin, TemplateView):
+    required_group = "Supervisor"
+    template_name = "supervisor/home_supervisor.html"
+
+
+@login_required
+def cadastrar_usuario(request):
+    is_supervisor = (
+        request.user.groups.filter(name="Supervisor").exists()
+        or request.user.is_superuser
+    )
+    if not is_supervisor:
+        raise PermissionDenied("Apenas Supervisores podem cadastrar novos usuários.")
+
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST, criado_por=request.user)
+        if form.is_valid():
+            user = form.save()
+            sincronizar_grupo(user)
+            messages.success(request, "Cadastro realizado com sucesso!")
+            return redirect("supervisor:painel")
+    else:
+        form = CustomUserCreationForm(criado_por=request.user)
+
+    return render(request, "supervisor/register.html", {"form": form})
