5781c3a4bd2438e52494d00170fb0360bb485921
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-08-05 16:34:48 -0300

    feat: cria área de atuçâo de professor

--- areas/models.py
+++ areas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class AreaAtuacao(models.Model):
+class AreaActing(models.Model):
     nome = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
