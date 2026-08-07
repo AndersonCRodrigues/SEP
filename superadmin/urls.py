4d6141a7929d3a0d6d58e24d6e737459dcbf05d8
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-07 10:36:08 -0300

    feat: adiciona rota para o perfil do superadmin

--- superadmin/urls.py
+++ superadmin/urls.py
@@ -6,4 +6,5 @@ app_name = "superadmin"
 urlpatterns = [
     path("", views.HomeSuperadminView.as_view(), name="home"),
     path("painel/", views.PainelSuperadminView.as_view(), name="painel"),
+    path("perfil/", views.PerfilSuperadminView.as_view(), name="perfil"),
 ]
