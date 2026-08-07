49509f52757ffeb680e27bcb5c9021c60ccd599c
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-07 16:03:23 -0300

    fix: ajusta a inicialização do SupervisorCreationForm para definir o papel como SUPERVISOR

--- supervisor/forms.py
+++ supervisor/forms.py
@@ -3,7 +3,10 @@ from teacher.forms import ProfessorCreationForm
 
 
 class SupervisorCreationForm(ProfessorCreationForm):
-    
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.role = CustomUser.Role.SUPERVISOR  
 
     def save(self, commit=True):
         supervisor = super().save(commit=False)
