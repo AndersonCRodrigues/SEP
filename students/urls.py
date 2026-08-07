b03986d1dc08dd4dba5a6ca0f7dca24b66e83d11
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-07 10:35:52 -0300

    feat: corrige rota do painel do estudante para usar a view correta

--- students/urls.py
+++ students/urls.py
@@ -5,5 +5,8 @@ app_name = "students"
 
 urlpatterns = [
     path("", views.HomeEstudanteView.as_view(), name="home"),
-    path("painel/", views.PainelEstudanteView.as_view(), name="painel"),
+    path("painel/", views.HomeEstudanteView.as_view(), name="painel"),
+    path("cadastrar/", views.cadastrar_aluno, name="cadastrar"),
+    path("perfil/", views.PerfilAlunoView.as_view(), name="perfil"),
+    path("meu-professor/", views.MeuProfessorView.as_view(), name="meu_professor"),
 ]
