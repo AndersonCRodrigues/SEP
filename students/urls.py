da76ca5e198483022c0123ef92e9aad7b0404c48
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-07 16:03:06 -0300

    fix: corrige a view do painel do estudante para usar PainelEstudanteView

--- students/urls.py
+++ students/urls.py
@@ -5,7 +5,7 @@ app_name = "students"
 
 urlpatterns = [
     path("", views.HomeEstudanteView.as_view(), name="home"),
-    path("painel/", views.HomeEstudanteView.as_view(), name="painel"),
+    path("painel/", views.PainelEstudanteView.as_view(), name="painel"), 
     path("cadastrar/", views.cadastrar_aluno, name="cadastrar"),
     path("perfil/", views.PerfilAlunoView.as_view(), name="perfil"),
     path("meu-professor/", views.MeuProfessorView.as_view(), name="meu_professor"),
