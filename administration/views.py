f0e6fbc60ad4ce91acc042f11cc55e246e539bec
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-07 10:33:33 -0300

    feat: implementa cadastro de pacientes e administrativos com validações de permissão

--- administration/views.py
+++ administration/views.py
@@ -1,20 +1,72 @@
-from django.views.generic import ListView, TemplateView
-from core.models import CustomUser
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from core.mixins import GroupRequiredMixin
+from .forms import AdministrativoCreationForm
+from core.utils import sincronizar_grupo
+from core.models import CustomUser
+from .forms import AdministrativoCreationForm
+from patient.forms import PacienteCreationForm
 
 
-class PainelAdministracaoView(GroupRequiredMixin, ListView):
+@login_required
+def cadastrar_paciente(request):
+    is_administrativo = request.user.is_superuser or request.user.role == CustomUser.Role.ADMINISTRATIVO
+    if not is_administrativo:
+        raise PermissionDenied("Apenas o Administrativo pode cadastrar Paciente.")
+
+    if request.method == "POST":
+        form = PacienteCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            sincronizar_grupo(user)
+            messages.success(request, "Paciente cadastrado com sucesso!")
+            return redirect("administration:painel")
+    else:
+        form = PacienteCreationForm()
+
+    return render(request, "administration/cadastrar_paciente.html", {"form": form})
+
+
+class PainelAdministracaoView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = "administration/painel.html"
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.role == CustomUser.Role.ADMINISTRATIVO
+
+
+class PerfilAdministrativoView(GroupRequiredMixin, UpdateView):
     required_group = "Administration"
     model = CustomUser
-    template_name = "administration/administration_panel.html"
-    context_object_name = "atendimentos_ou_usuarios"
+    fields = [
+        "nome_completo", "telefone",
+        "logradouro", "numero", "complemento",
+        "bairro", "cidade", "estado", "cep",
+    ]
+    template_name = "administration/perfil.html"
+    success_url = reverse_lazy("administration:perfil")
 
-    def get_queryset(self):
-        return CustomUser.objects.filter(
-            role__in=[CustomUser.Role.ALUNO, CustomUser.Role.PROFESSOR]
-        )
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
-class HomeAdministracaoView(GroupRequiredMixin, TemplateView):
-    required_group = "Administration"
-    template_name = "administration/home_administration.html"
+@login_required
+def cadastrar_administrativo(request):
+    if not request.user.is_superuser:
+        raise PermissionDenied("Apenas o Superadmin pode cadastrar Administrativo.")
+
+    if request.method == "POST":
+        form = AdministrativoCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            sincronizar_grupo(user)
+            messages.success(request, "Administrativo cadastrado com sucesso!")
+            return redirect("superadmin:painel")  # era administration:painel
+    else:
+        form = AdministrativoCreationForm()
+
+    return render(request, "administration/cadastro.html", {"form": form})
