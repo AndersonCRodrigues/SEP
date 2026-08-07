07b70a89491589269eac5d1a38799fcf895b2923
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-07 16:02:52 -0300

    fix: corrige a importação do UserCreationForm no formulário de criação de paciente

--- patient/forms.py
+++ patient/forms.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.forms import UserCreationForm
 from core.models import CustomUser
-from core.forms import UserCreationForm
+
 
 class PacienteCreationForm(UserCreationForm):
     class Meta:
@@ -10,9 +11,13 @@ class PacienteCreationForm(UserCreationForm):
             "cep",
         )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.instance.role = CustomUser.Role.PACIENTE 
+
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.role = CustomUser.Role.PACIENTE
+        user.role = CustomUser.Role.PACIENTE  
         if commit:
             user.save()
         return user
