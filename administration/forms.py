36ec4f1e2006cb6482cbfbfd84a5e2363bf5cbe7
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-07 10:33:16 -0300

    feat: adiciona formulário de criação para usuários administrativos

---
+++ administration/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from core.models import CustomUser
+
+
+class AdministrativoCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "email", "nome_completo", "cpf", "telefone", "logradouro",
+            "numero", "complemento", "bairro", "cidade", "estado",
+            "cep", "matricula",
+        )
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.role = CustomUser.Role.ADMINISTRATIVO  # era ADMIN
+        if commit:
+            user.save()
+        return user
+
+    def clean_matricula(self):
+        matricula = self.cleaned_data.get("matricula")
+        if not matricula:
+            raise forms.ValidationError("Matrícula é obrigatória.")
+        return matricula
