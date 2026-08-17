a5439ffd15df54818d60ccf63ffc54038c8082df
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-17 12:18:02 -0300

    feat:adição de novos campos

--- patient/forms.py
+++ patient/forms.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.forms import UserCreationForm
 from core.models import CustomUser
 from .models import Patient
+from django import forms
 
 
 class PacienteCreationForm(UserCreationForm):
+    
+    data_nascimento = forms.DateField(
+    label="Data de nascimento",
+    widget=forms.DateInput(
+        attrs={"class": "form-control date-picker", "autocomplete": "off"},
+        format="%Y-%m-%d",
+    ),
+    input_formats=["%Y-%m-%d"],
+)
     class Meta:
         # Patient, nao CustomUser: sem a linha filha da heranca multi-tabela o
         # paciente nao existe para Patient.objects nem para as FKs que o apontam.
@@ -12,6 +22,9 @@ class PacienteCreationForm(UserCreationForm):
             "email",
             "nome_completo",
             "cpf",
+            "social_name",
+            "gender_identity",
+            "data_nascimento",
             "telefone",
             "logradouro",
             "numero",
@@ -21,14 +34,22 @@ class PacienteCreationForm(UserCreationForm):
             "estado",
             "cep",
         )
+        
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.instance.role = CustomUser.Role.PACIENTE
+        self.fields.pop("password1",None)
+        self.fields.pop("password2",None)
 
     def save(self, commit=True):
-        user = super().save(commit=False)
-        user.role = CustomUser.Role.PACIENTE
-        if commit:
-            user.save()
-        return user
+        if not commit:
+            raise NotImplementedError(
+                "PacienteCreationForm sempre precisa gerar credenciais; "
+                "não há suporte a commit=False."
+            )
+        cleaned = {
+            field: self.cleaned_data.get(field)
+            for field in self.Meta.fields
+        }
+        return Patient.objects.create_with_credentials(**cleaned)
