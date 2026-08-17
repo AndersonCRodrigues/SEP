95caedcbd5384a5e74e23843e7941936f5a16fa5
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-17 12:22:18 -0300

    feat:adição da lógica de role supervisor

--- supervisor/forms.py
+++ supervisor/forms.py
@@ -1,15 +1,34 @@
-from core.models import CustomUser
+from django import forms
+from core.forms import PERSONAL_FIELDS
+from teacher.models import Teacher
 from teacher.forms import ProfessorCreationForm
 
 
 class SupervisorCreationForm(ProfessorCreationForm):
+    class Meta:
+        model = Teacher
+        fields = PERSONAL_FIELDS
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.instance.role = CustomUser.Role.SUPERVISOR
+        self.fields["role"].choices = [
+            (value, label)
+            for value, label in Teacher.Role.choices
+            if value == Teacher.Role.SUPERVISOR
+        ]
+        self.initial["role"] = Teacher.Role.SUPERVISOR
+
+    def clean_role(self):
+        role = self.cleaned_data["role"]
+        if role != Teacher.Role.SUPERVISOR:
+            raise forms.ValidationError(
+                "Pelo Superadmin, só é possível cadastrar Supervisor."
+            )
+        return role
 
     def save(self, commit=True):
         supervisor = super().save(commit=False)
-        supervisor.role = CustomUser.Role.SUPERVISOR
+        supervisor.role = Teacher.Role.SUPERVISOR
         if commit:
             supervisor.save()
         return supervisor
