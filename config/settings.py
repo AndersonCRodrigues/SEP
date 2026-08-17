8872b62f322b4ee81dfc4636584fcee104d56f3c
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-17 12:09:56 -0300

    feat(config): adição da configuração do email disparado

--- config/settings.py
+++ config/settings.py
@@ -172,6 +172,24 @@ else:
     }
 
 
+EMAIL_BACKEND = os.getenv(
+    "DJANGO_EMAIL_BACKEND",
+    "django.core.mail.backends.console.EmailBackend",
+)
+
+
+
+DEFAULT_FROM_EMAIL = os.getenv("DJANGO_DEFAULT_FROM_EMAIL","no-reply@sep.local")
+
+
+if EMAIL_BACKEND == "django.core.mail.backends.smtp.EmailBackend":
+    EMAIL_HOST = os.getenv("DJANGO_EMAIL_HOST")
+    EMAIL_PORT = int(os.getenv("DJANGO_EMAIL_PORT", "587"))
+    EMAIL_HOST_USER = os.getenv("DJANGO_EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.getenv("DJANGO_EMAIL_HOST_PASSWORD")
+    EMAIL_USE_TLS = os.getenv("DJANGO_EMAIL_USE_TLS", "True").lower() == "true"
+
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": (
