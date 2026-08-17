81b2741651ef627a003b419053122c28ec77cb72
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-08-17 12:11:19 -0300

    refactor:alterando admin para superadmin

--- core/forms.py
+++ core/forms.py
@@ -97,6 +97,6 @@ class SupervisorCreationForm(UserCreationForm):
         role = self.cleaned_data["role"]
         if role != CustomUser.Role.SUPERVISOR:
             raise forms.ValidationError(
-                "Pelo admin, só é possível cadastrar Supervisor."
+                "Pelo Superadmin, só é possível cadastrar Supervisor."
             )
         return role
