d348176f0b9a54261112b0c8b1197b5f57c4e6aa
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-09-24 12:37:37 -0300

    refactor: remove senha e endereço para cadastro parcial

--- teacher/forms.py
+++ teacher/forms.py
@@ -33,8 +33,9 @@ class VincularAlunoForm(forms.Form):
 
 
 class ProfessorCreationForm(UserCreationForm):
-    # Teacher.clean() exige ao menos uma area: opcional aqui passava a
-    # validacao e so estourava depois.
+    password1 = None
+    password2 = None
+
     acting_areas = forms.ModelMultipleChoiceField(
         queryset=AreaActing.objects.all(),
         widget=forms.CheckboxSelectMultiple,
@@ -50,13 +51,6 @@ class ProfessorCreationForm(UserCreationForm):
             "cpf",
             "telefone",
             "data_nascimento",
-            "logradouro",
-            "numero",
-            "complemento",
-            "bairro",
-            "cidade",
-            "estado",
-            "cep",
             "matricula",
             "crp",
             "acting_areas",
@@ -67,7 +61,7 @@ class ProfessorCreationForm(UserCreationForm):
         self.instance.role = CustomUser.Role.PROFESSOR
 
     def save(self, commit=True):
-        professor = super().save(commit=False)
+        professor = forms.ModelForm.save(self, commit=False)
         professor.role = CustomUser.Role.PROFESSOR
         if commit:
             professor.save()
