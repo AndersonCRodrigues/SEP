c1a3e36ecd373ff79308429bc23b13ea2aed8bbb
Grediness <168847376+PedroDefanti@users.noreply.github.com>
2026-09-25 10:07:47 -0300

    fix(urls): corrige nomes de funções nas rotas de criação de triagem

--- triage/urls.py
+++ triage/urls.py
@@ -1,30 +1,24 @@
 from django.urls import path
 from . import views
 
+app_name = "triage"
+
 
 urlpatterns = [
     path("minhas-triagens/", views.minhas_triagens, name="minhas_triagens"),
     path("fila/", views.fila_triagem, name="fila_triagem"),
-    path("create/<int:patient_id>/", views.create_triage, name="create_triage"),
+    path("create/<int:patient_id>/", views.create_triage_start, name="create_triage"),
+    path("create/<int:patient_id>/<slug:step>/", views.create_triage_step, name="create_triage_step"),
     path("concluida/<int:pk>/", views.triagem_concluida, name="triagem_concluida"),
     path("edit/<int:pk>/", views.edit_triage, name="edit_triage"),
     path("submit/<int:pk>/", views.submit_triage, name="submit_triage"),
     path("feedback/<int:pk>/", views.create_feedback, name="create_feedback"),
     path("create_referral/<int:pk>/", views.create_referral, name="create_referral"),
+    
     # Rotas separadas por Role
-    path(
-        "student_detail/<int:pk>/",
-        views.triage_detail_student,
-        name="triage_detail_student",
-    ),
-    path(
-        "supervisor_detail/<int:pk>/",
-        views.triage_detail_supervisor,
-        name="triage_detail_supervisor",
-    ),
-    path(
-        "lock_triage_editing/<int:pk>/",
-        views.lock_triage_editing,
-        name="lock_triage_editing",
-    ),
+    path("student_detail/<int:pk>/", views.triage_detail_student, name="triage_detail_student"),
+    path("supervisor_detail/<int:pk>/", views.triage_detail_supervisor, name="triage_detail_supervisor"),
+    
+    path("lock_triage_editing/<int:pk>/", views.lock_triage_editing, name="lock_triage_editing"),
+    
 ]
